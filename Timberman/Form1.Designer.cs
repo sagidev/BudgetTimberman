b6019b0299872281eb80b32c89abe716e5ee97ad
sagidev <sagithereal@gmail.com>
2022-04-27 13:21:12 +0200

    upload

---
+++ Timberman/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Timberman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.playerBox = new System.Windows.Forms.PictureBox();
+            this.scoreLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.moneyLbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.highscoreLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.pictureBox1.Location = new System.Drawing.Point(400, -15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 680);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // playerBox
+            // 
+            this.playerBox.BackColor = System.Drawing.Color.Cyan;
+            this.playerBox.Location = new System.Drawing.Point(284, 570);
+            this.playerBox.Name = "playerBox";
+            this.playerBox.Size = new System.Drawing.Size(97, 81);
+            this.playerBox.TabIndex = 2;
+            this.playerBox.TabStop = false;
+            // 
+            // scoreLbl
+            // 
+            this.scoreLbl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.scoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scoreLbl.ForeColor = System.Drawing.Color.White;
+            this.scoreLbl.Location = new System.Drawing.Point(400, 0);
+            this.scoreLbl.Name = "scoreLbl";
+            this.scoreLbl.Size = new System.Drawing.Size(116, 124);
+            this.scoreLbl.TabIndex = 3;
+            this.scoreLbl.Text = "0";
+            this.scoreLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 53);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Money:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // moneyLbl
+            // 
+            this.moneyLbl.BackColor = System.Drawing.Color.MidnightBlue;
+            this.moneyLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.moneyLbl.ForeColor = System.Drawing.Color.White;
+            this.moneyLbl.Location = new System.Drawing.Point(119, 0);
+            this.moneyLbl.Name = "moneyLbl";
+            this.moneyLbl.Size = new System.Drawing.Size(134, 53);
+            this.moneyLbl.TabIndex = 5;
+            this.moneyLbl.Text = "0";
+            this.moneyLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 53);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Highscore:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // highscoreLbl
+            // 
+            this.highscoreLbl.BackColor = System.Drawing.Color.MidnightBlue;
+            this.highscoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.highscoreLbl.ForeColor = System.Drawing.Color.White;
+            this.highscoreLbl.Location = new System.Drawing.Point(171, 48);
+            this.highscoreLbl.Name = "highscoreLbl";
+            this.highscoreLbl.Size = new System.Drawing.Size(82, 53);
+            this.highscoreLbl.TabIndex = 7;
+            this.highscoreLbl.Text = "0";
+            this.highscoreLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(923, 663);
+            this.Controls.Add(this.highscoreLbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.moneyLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreLbl);
+            this.Controls.Add(this.playerBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox playerBox;
+        private System.Windows.Forms.Label scoreLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label moneyLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label highscoreLbl;
+    }
+}
+
---
+++ Timberman/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Timberman
+{
+    public partial class Form1 : Form
+    {
+        static int money = 0;
+        static int branchesAmount = 500;
+        //int[] branches = new int[100];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartGame();
+            //spawnBranches();
+
+        }
+
+        void GameOver()
+        {
+            MessageBox.Show("Score: " + score);
+            if (score > highscore)
+                highscore = score;
+            highscoreLbl.Text = highscore.ToString();
+            money = money + score;
+            StartGame();
+        }
+        int highscore = 0;
+        int score = 0;
+        void UpdateGame()
+        {
+            scoreLbl.Text = score.ToString();
+            foreach(var branch in branches)
+            {
+               
+                if (branch!= null)
+                {
+                    branch.Location = new Point(branch.Location.X, branch.Location.Y + 100);
+                    if (playerBox.Location.X == 284 && branch.Location.X == 256 && branch.Location.Y == 568)
+                    {
+                        GameOver();
+                    }
+                    if (playerBox.Location.X == 536 && branch.Location.X == 512 && branch.Location.Y == 568)
+                    {
+                        GameOver();
+                    }
+                    if(branch.Location.Y > 600)
+                    {
+                        Controls.Remove(branch);
+                    }
+                    //Controls.Add(branch);
+                }//284; 570
+
+            }
+            moneyLbl.Text = money.ToString();
+            score++;
+        }
+
+        void newUpdateGame()
+        {
+            scoreLbl.Text = score.ToString();
+            foreach (var branch in newBranches)
+            {
+
+                if (branch != null)
+                {
+                    branch.Location = new Point(branch.Location.X, branch.Location.Y + 100);
+                    if (playerBox.Location.X == 284 && branch.Location.X == 256 && branch.Location.Y == 568)
+                    {
+                        GameOver();
+                    }
+                    if (playerBox.Location.X == 536 && branch.Location.X == 512 && branch.Location.Y == 568)
+                    {
+                        GameOver();
+                    }
+                    if (branch.Location.Y > 600)
+                    {
+                        Random rng = new Random();
+                        int rnd = rng.Next(0, 2);
+                        if(rnd == 0)
+                        {
+                            branch.Location = new Point(256, 68);
+                        }
+                        else
+                        {
+                            branch.Location = new Point(512, 68);
+                        }
+                        
+                    }
+                    //Controls.Add(branch);
+                }//284; 570
+
+            }
+            moneyLbl.Text = money.ToString();
+            score++;
+        }
+
+        void StartGame()
+        {
+            scoreLbl.Text = score.ToString();
+            foreach (var branch in newBranches)
+            {
+                Controls.Remove(branch);
+            }
+            Array.Clear(newBranches, 0, newBranches.Length);
+            score = 0;
+            //spawnBranches();
+            newSpawn();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (e.KeyCode == Keys.L)
+            //{
+            //    //Do here
+            //}
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                //location = 0;
+                playerBox.Location = new Point(284, 570);
+                //spawnBranches();
+                newUpdateGame();
+                
+                
+
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                //location = 1;
+                playerBox.Location = new Point(536,570);
+                newUpdateGame();
+            }
+
+            //536; 570
+        }
+        PictureBox[] branches = new PictureBox[branchesAmount];
+        PictureBox[] newBranches = new PictureBox[6];
+
+        void newSpawn()
+        {
+            Random rng = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                int rnd = rng.Next(0, 2);
+                if (rnd == 0)
+                {
+                    var picture = new PictureBox
+                    {//lewa
+                        Name = ("pictureBox" + i),
+                        Size = new Size(145, 40),
+                        Location = new Point(256, 468 - (i * 100)),
+                        BackColor = Color.FromArgb(128, 64, 0),
+                    };
+                    this.Controls.Add(picture);
+                    newBranches[i] = picture;
+                }
+                else if (rnd == 1)
+                {//prawa
+                    var picture = new PictureBox
+                    {
+                        Name = ("pictureBox" + i),
+                        Size = new Size(145, 40),
+                        Location = new Point(512, 468 - (i * 100)),
+                        BackColor = Color.FromArgb(128, 64, 0),
+                    };
+                    this.Controls.Add(picture);
+                    newBranches[i] = picture;
+                }
+            }
+        }
+        void spawnBranches()
+        {
+            Random rng = new Random();
+            for (int i = 0; i< branchesAmount; i++)
+            {
+                //PictureBox box = new PictureBox();
+                
+                //box.Size = new Size(144, 50);
+                int rnd = rng.Next(0, 2);
+                if (rnd == 0)
+                {
+                    var picture = new PictureBox
+                    {//lewa
+                        Name = ("pictureBox" + i),
+                        Size = new Size(145, 40),
+                        Location = new Point(256, 468 - (i * 100)),
+                        BackColor = Color.FromArgb(128, 64, 0),
+                        
+                        //Image = Image.FromFile("hello.jpg"),
+
+                    };
+                    this.Controls.Add(picture);
+                    branches[i] = picture;
+                }
+                else if (rnd == 1)
+                {//prawa
+                    var picture = new PictureBox
+                    {
+                        Name = ("pictureBox" + i),
+                        Size = new Size(145, 40),
+                        Location = new Point(512, 468 - (i * 100)),
+                        BackColor = Color.FromArgb(128, 64, 0),
+                        //Image = Image.FromFile("hello.jpg"),
+
+                    };
+                    this.Controls.Add(picture);
+                    branches[i] = picture;
+                    //box.Size = new Size(144, 50);
+
+                }
+            }
+        }
+
+        private void branchBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
